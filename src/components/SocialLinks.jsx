20fd40666f4433b880e6ecf1fe501374bd9c24d6
Sidhart Singh <isidhartsingh@gmail.com>
2024-05-29 08:47:39 +0530

    add links to socail icons

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,17 +1,62 @@
-import { HStack, Icon } from "@chakra-ui/react";
 import React from "react";
+import { HStack, IconButton } from "@chakra-ui/react";
 import { SiLeetcode } from "react-icons/si";
 import { BiLogoGmail } from "react-icons/bi";
 import { BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+const links = {
+  linkedIn: "https://linkedin.com/in/sidhart-singh",
+  leetcode: "https://leetcode.com/u/i_SID/",
+  github: "https://github.com/sidhart-singh",
+  gmail: "mailto:isidhartsingh@gmail.com",
+  instagram: "",
+};
 
 const SocialLinks = () => {
   return (
-    <HStack gap={6}>
-      <Icon as={BsLinkedin} fontSize={"14px"} boxSize={"20px"} color={"gray"} />
-      <Icon as={SiLeetcode} boxSize={"20px"} color={"gray"} />
-      <Icon as={BsGithub} boxSize={"20px"} color={"gray"} />
-      <Icon as={BiLogoGmail} boxSize={"22px"} color={"gray"} />
-      <Icon as={BsInstagram} boxSize={"20px"} color={"gray"} />
+    <HStack gap={2}>
+      <IconButton
+        bg={"transparent"}
+        as={Link}
+        to={links.gmail}
+        icon={<BiLogoGmail size={"md"} />}
+        boxSize={"24px"}
+        color={"gray.600"}
+      />
+
+      <IconButton
+        bg={"transparent"}
+        as={Link}
+        to={links.linkedIn}
+        icon={<BsLinkedin size={"sm"} />}
+        boxSize={"20px"}
+        color={"gray.600"}
+      />
+      <IconButton
+        bg={"transparent"}
+        as={Link}
+        to={links.github}
+        icon={<BsGithub size={"md"} />}
+        boxSize={"20px"}
+        color={"gray.600"}
+      />
+      <IconButton
+        bg={"transparent"}
+        as={Link}
+        to={links.leetcode}
+        icon={<SiLeetcode size={"md"} />}
+        boxSize={"20px"}
+        color={"gray.600"}
+      />
+      <IconButton
+        bg={"transparent"}
+        as={Link}
+        to={links.instagram}
+        icon={<BsInstagram size={"md"} />}
+        boxSize={"20px"}
+        color={"gray.600"}
+      />
     </HStack>
   );
 };
